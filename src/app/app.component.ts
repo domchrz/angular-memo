888be9abbd2b0f216ce9dca93cf34b792447850d
myszitsu <d.chrzescijanek@gmail.com>
2022-02-17 13:53:55 +0100

    added core flipping and matching logic using animations callbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CARDS } from './constants/cards';
 import { Card } from './models/cards';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -10,34 +16,70 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('flip', [
-      state('faced-up', style({
-        transform: 'rotateX(180deg)'
-      })),
-      state('faced-down', style({
-        transform: 'rotateX(0)'
-      })),
-      transition('faced-up <=> faced-down', animate(700))
-    ])
-  ]
+      state(
+        'faced-up',
+        style({
+          transform: 'rotateX(180deg)',
+        })
+      ),
+      state(
+        'faced-down',
+        style({
+          transform: 'rotateX(0)',
+        })
+      ),
+      transition('faced-up <=> faced-down', animate(700)),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'angular-memo';
-  cards: Card[] = []
+  cards: Card[] = [];
+  flippedCards: Card[] = [];
+  length: number = 0;
+  hasLoaded = false;
+  score: number = 0;
+  movesMade: number = 0;
 
   ngOnInit() {
-    CARDS.forEach(card => {
-      this.cards.push({...card})
-      this.cards.push({...card})
-    })
-    this.cards.sort((a, b) => .5 - Math.random())
+    CARDS.forEach((card) => {
+      this.cards.push({ ...card }, { ...card });
+    });
+    this.cards.sort((a, b) => 0.5 - Math.random());
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 0);
   }
 
   flip(card: Card) {
-    card.isFlipped = !card.isFlipped
-    console.log(card);
+    card.isFlipped = !card.isFlipped;
   }
 
-  clg() {
-    console.log('start')
+  flipStart() {
+    this.flippedCards = this.cards.filter(
+      (card) => card.isFlipped === true && card.isMatched === false
+    );
+    if (this.flippedCards.length === 2) {
+      this.movesMade += 1;
+    }
+  }
+
+  flipEnd() {
+    if (this.flippedCards.length !== 2) {
+      return;
+    }
+    
+    this.flippedCards[0].id === this.flippedCards[1].id
+      ? this.flippedCards.forEach((card) => (card.isMatched = true))
+      : false;
+    this.score =
+      this.cards.filter((card) => card.isMatched === true).length / 2;
+
+    setTimeout(() => {
+      this.flippedCards = [];
+      this.cards.forEach((card) => {
+        !card.isMatched ? (card.isFlipped = false) : false;
+      });
+    }, 1000);
   }
 }
